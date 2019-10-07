a93c55c46fa075d664a526281d551c5d2a38a6f5
Stephen Baxendale <sbaxenda@gmail.com>
2019-10-07 23:15:57 +1000

    Hardcoded wss @ 8051 with echo message implementation

---
+++ NodeMessageHandlers.js
@@ -0,0 +1,44 @@
+/*\
+title: $:/plugins/sbaxenda/tiddly-websocket-recorder/NodeMessageHandlers.js
+type: application/javascript
+module-type: startup
+
+  These are message handler functions for the web socket servers. Use this file
+  as a template for extending the web socket functions.
+
+  This handles messages sent to the node process.
+\*/
+(function(){
+
+    /*jslint node: true, browser: true */
+    /*global $tw: false */
+    "use strict";
+
+    // This lets you add to the $tw.nodeMessageHandlers object without overwriting
+    // existing handler functions
+    $tw.nodeMessageHandlers = $tw.nodeMessageHandlers || {};
+
+    /*
+      This is just a test function to make sure that everthing is working.
+      It just displays the contents of the data in the console.
+    */
+    $tw.nodeMessageHandlers.test = function(data) {
+        console.log("nodeMessageHandlers.test -->");
+        console.log(data);
+        console.log("<--");
+    }
+
+    $tw.nodeMessageHandlers.echo = function(data) {
+        console.log("nodeMessageHandlers.echo -->");
+        $tw.connections[0].socket.send(JSON.stringify(data));
+        console.log(data);
+        console.log("<--");
+    }
+
+    $tw.nodeMessageHandlers.testsjb = function(data) {
+        console.log("nodeMessageHandlers.testsjb -- sjb -->");
+        console.log(data);
+        console.log("<-- sjb --");
+    }
+
+})()
